5563a1cd35edcc6693782c3b096c39dd300d3d35
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2020-10-02 03:02:28 -0700

    Extra captioning levels

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES="$1" python -m scripts.caption --cuda
+# Default - simple goto
+# CUDA_VISIBLE_DEVICES="$1" python -m scripts.caption --cuda --exp_dir exp/goto
+
+# Default - simple pickup
+# CUDA_VISIBLE_DEVICES="$1" python -m scripts.caption --cuda --exp_dir exp/pickup --demos demos/BabyAI-Pickup-v0.pkl
+
+# CUDA_VISIBLE_DEVICES="$1" python -m scripts.caption --cuda --exp_dir exp/goto-pickup --demos demos/BabyAI-Pickup-v0.pkl demos/BabyAI-GoTo-v0.pkl
+
+# CUDA_VISIBLE_DEVICES="$1" python -m scripts.caption --cuda --exp_dir exp/goto-pickup-putnext --demos demos/BabyAI-Pickup-v0.pkl demos/BabyAI-GoTo-v0.pkl demos/BabyAI-PutNext-v0.pkl
+
+CUDA_VISIBLE_DEVICES="$1" python -m scripts.caption --cuda --exp_dir exp/boss --demos demos/BabyAI-BossLevel-v0.pkl --n_per_file 500000

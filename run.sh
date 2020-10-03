d193ccbabb8e6b113042c1dcd1e47032c0169187
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2020-10-02 17:45:31 -0700

    big bosslevel

--- run.sh
+++ run.sh
@@ -10,4 +10,6 @@
 
 # CUDA_VISIBLE_DEVICES="$1" python -m scripts.caption --cuda --exp_dir exp/goto-pickup-putnext --demos demos/BabyAI-Pickup-v0.pkl demos/BabyAI-GoTo-v0.pkl demos/BabyAI-PutNext-v0.pkl
 
-CUDA_VISIBLE_DEVICES="$1" python -m scripts.caption --cuda --exp_dir exp/boss --demos demos/BabyAI-BossLevel-v0.pkl --n_per_file 500000
+# CUDA_VISIBLE_DEVICES="$1" python -m scripts.caption --cuda --exp_dir exp/boss --demos demos/BabyAI-BossLevel-v0.pkl --n_per_file 500000
+
+CUDA_VISIBLE_DEVICES="$1" python -m scripts.caption --cuda --exp_dir exp/boss_big --demos demos/BabyAI-BossLevel-v0.pkl --n_per_file 1000000
